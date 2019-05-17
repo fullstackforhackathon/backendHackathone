4ace098dd608a45ea35164fe155347d7ffb4bc71
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-18 02:34:09 +0700

    qwe

--- backendHackathone/Controllers/ValuesController.cs
+++ backendHackathone/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace backendHackathone.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "hello", "from", "travis", "2" };
+            return new string[] { "hello", "from", "travis", "3" };
         }
 
         // GET api/values/5
