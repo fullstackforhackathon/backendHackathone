eecde865a7dee8bfd26aab9d15d577277ae30f7b
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-18 02:02:18 +0700

    restart service from travis

--- backendHackathone/Controllers/ValuesController.cs
+++ backendHackathone/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace backendHackathone.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "hello", "from", "travis" };
         }
 
         // GET api/values/5
