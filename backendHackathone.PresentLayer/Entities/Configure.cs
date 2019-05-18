e25851b9750ffbd38941e5f148326292e6889a69
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-19 03:40:24 +0700

    Add Configure entity

---
+++ backendHackathone.PresentLayer/Entities/Configure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace backendHackathone.PresentLayer.Entities
+{
+    public class Configure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
