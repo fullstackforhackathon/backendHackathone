9f12c02188dd2578ef53cae48bff5316ecb8fe64
Jaroslav Pavlenko <jaroslav.ender@yandex.ru>
2019-05-19 04:35:28 +0700

    Added DbContext

---
+++ backendHackathone.Core/Contexts/EntityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using backendHackathone.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendHackathone.Core.Contexts
+{
+    public class EntityContext : DbContext
+    {
+        public DbSet<BusinessEntityType> BusinessEntityTypes { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<FieldValue> FieldValues { get; set; }
+        public DbSet<PowerLine> PowerLinesData { get; set; }
+
+        public EntityContext(DbContextOptions<EntityContext> options)
+            : base(options) { }
+    }
+}
---
+++ backendHackathone.Core/Entities/PowerLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backendHackathone.Core.Entities
+{
+    public class PowerLine : IBusinessEntity
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string DispatchNumber { get; set; }
+        public string Name { get; set; }
+        public int Voltage { get; set; }
+        public int CommissioningYear { get; set; }
+        public int NumberOfChains { get; set; }
+        public double SummaryLength { get; set; }
+        public double SummaryLengthByChain { get; set; }
+        public double Length { get; set; }
+        public double LengthByChain { get; set; }
+        public string LineMark { get; set; }
+        public string TechnicalStatus { get; set; }
+    }
+}
