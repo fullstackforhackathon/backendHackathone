364bb28507aa7e118ef30a2b6f89e504b968757e
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-19 03:49:28 +0700

    add FieldsMappingRules

---
+++ backendHackathone.PresentLayer/Entities/FieldsMappingRules.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backendHackathone.PresentLayer.Entities
+{
+    public class FieldsMappingRules
+    {
+        public int ConfigurationId { get; set; }
+        public int OriginField { get; set; }
+        public int SourceField { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
