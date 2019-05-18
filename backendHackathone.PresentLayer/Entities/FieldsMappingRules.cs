d6fdcca15c7c293ccce11e6cd7aea0cbfe0cb007
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-19 05:09:35 +0700

    add first migration

--- backendHackathone.PresentLayer/Entities/FieldsMappingRules.cs
+++ backendHackathone.PresentLayer/Entities/FieldsMappingRules.cs
@@ -4,6 +4,7 @@ namespace backendHackathone.PresentLayer.Entities
 {
     public class FieldsMappingRules
     {
+        public int Id { get; set; }
         public int ConfigurationId { get; set; }
         public bool IsVisible { get; set; }
 
