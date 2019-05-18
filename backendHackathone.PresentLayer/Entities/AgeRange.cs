a9b6c8499324fec20e56adc50d0703299776310d
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-19 03:42:16 +0700

    Add AgeRange entity

---
+++ backendHackathone.PresentLayer/Entities/AgeRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backendHackathone.PresentLayer.Entities
+{
+    public class AgeRange
+    {
+        public int ConfigureId { get; set; }
+        public int Id { get; set; }
+        public int StartAge { get; set; }
+        public int EndAge { get; set; }
+    }
+}
