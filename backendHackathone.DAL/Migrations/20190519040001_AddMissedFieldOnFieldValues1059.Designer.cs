f64b804854d3d1dd838647619f9de3cbfdb5a6ce
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-19 11:03:58 +0700

    fix migration

--- backendHackathone.DAL/Migrations/20190519040001_AddMissedFieldOnFieldValues1059.Designer.cs
+++ backendHackathone.DAL/Migrations/20190519040001_AddMissedFieldOnFieldValues1059.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backendHackathone.DAL.Contexts;
 
 namespace backendHackathone.DAL.Migrations
 {
     [DbContext(typeof(EntityContext))]
-    partial class EntityContextModelSnapshot : ModelSnapshot
+    [Migration("20190519040001_AddMissedFieldOnFieldValues1059")]
+    partial class AddMissedFieldOnFieldValues1059
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
